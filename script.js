9f410476f545db6295637108f70caa7352c1094a
Princewill Ugochukwu <flexyugoh081@gmail.com>
2024-12-13 00:56:11 +0100

    My Portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// Add event listener to button click event
+document.getElementById('myButton2').addEventListener('click', function() {
+    window.location.href = 'https://flexydolist.netlify.app/';
+});
+
+
+
+
+document.getElementById('myButton').addEventListener('click', function() {
+    window.location.href = 'https://flexyweatherapp.netlify.app/';
+});
